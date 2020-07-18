9e8d73d86a6aa811834b19c4ce203ffb6698f0b0
mav1283 <pdullano@gmail.com>
2020-07-19 01:04:36 +0800

    adding and updating more uis

--- src/components/layout/section_templates/FAQcontact.js
+++ src/components/layout/section_templates/FAQcontact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FAQcontact(props){
     return (
-        <section className='faq-contact'>
+        <section className='faq-contact sect'>
             <div className="faq-headline">
             <h1>Need some answers?</h1>
             <p>If you’ve never offshored before, you’re sure to have a lot of questions. We will be happy to answer your questions about from pricing models, setting up a team, to anything you need to know about outsourcing in the Philippines.</p>
@@ -14,10 +14,33 @@ function FAQcontact(props){
                 <label htmlFor="" className="form-group"><input type="text" placeholder="Your company"/></label>
                 <label htmlFor="" className="form-group"><input type="text" placeholder="Phone"/></label>
                 <label htmlFor="" className="form-group"><input type="text" placeholder="Email"/></label>
-                <label htmlFor="" className="form-group"><input type="text" placeholder="Company size"/></label>
-                <label htmlFor="" className="form-group"><input type="text" placeholder="Number of staff"/></label>
-                <label htmlFor="" className="form-group"><input type="text" placeholder="What country?"/></label>
-                <label htmlFor="" className="form-group"><input type="text" placeholder="Question"/></label>
+                <label htmlFor="" className="form-group">
+                    <select placeholder="Company size">
+                        <option value='lorem ipusm'>lorem ipsum</option>
+                        <option value='lorem ipsum'>lorem ipsume</option>
+                    </select>
+                </label>
+                <label htmlFor="" className="form-group">
+                    <select placeholder="Number of staff">
+                        <option value='lorem ipsum'>lorem ipsume</option>
+                        <option value='lorem ipsum'>lorem ipsume</option>
+                    </select>
+                </label>
+                <label htmlFor="" className="form-group">
+                    <select placeholder="What country?">
+                        <option value='Philippines'>Philippines</option>
+                        <option value='United Kingdom'>United Kingdom</option>
+                    </select>
+                </label>
+                <label htmlFor="" className="form-group"><textarea placeholder="Question"></textarea></label>
+                <label htmlFor="" className="form-group check">
+                    <input type="checkbox"/><span>We value your privacy. You agree to MicroSourcing's collection statement</span>
+                </label>
+                <label htmlFor="" className="form-group check">
+                    <input type="checkbox"/><span>Yes, I'd like free access to an in-depth course about offshoring to the Philippines.
+</span>
+                </label>
+                <button>Send Email</button>
             </form>
         </section>
     )
