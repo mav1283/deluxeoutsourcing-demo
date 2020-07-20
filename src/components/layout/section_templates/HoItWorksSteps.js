aacb59550b57c396d3e55e63199f4d2b8ea47a92
mav1283 <pdullano@gmail.com>
2020-07-21 00:18:52 +0800

    updating responsiveness

--- src/components/layout/section_templates/HoItWorksSteps.js
+++ src/components/layout/section_templates/HoItWorksSteps.js
@@ -1,43 +1,19 @@
-import React from 'react';
+import React from "react";
+import HiwStep from "./HiwStep";
 
-function HowItWorksSteps(props){
-    return (
-        <section className='hiw-steps sect'>
-            <div>
-                <p className="hiws-num">1</p>
-                <h1 className="hiws-step">Step Name</h1>
-                <p className="hiws-desc">Description text</p>
-                <div className="hiws-icon">icon here</div>
-                <ul className="hiws-lists">
-                <li>list item</li>
-                    <li>list item</li>
-                    <li>list item</li>
-                </ul>
-            </div>
-            <div>
-            <p className="hiws-num">2</p>
-                <h1 className="hiws-step">Step Name</h1>
-                <p className="hiws-desc">Description text</p>
-                <div className="hiws-icon">icon here</div>
-                <ul className="hiws-lists">
-                <li>list item</li>
-                    <li>list item</li>
-                    <li>list item</li>
-                </ul>
-            </div>
-            <div>
-            <p className="hiws-num">3</p>
-                <h1 className="hiws-step">Step Name</h1>
-                <p className="hiws-desc">Description text</p>
-                <div className="hiws-icon">icon here</div>
-                <ul className="hiws-lists">
-                    <li>list item</li>
-                    <li>list item</li>
-                    <li>list item</li>
-                </ul>
-            </div>
-        </section>
-    )
+function HowItWorksSteps({ hiwsteps }) {
+  return (
+    <section className="hiw-steps sect">
+      <div className="hiw-steps-headline">
+        <h1>Lorem Ipsum Dolor Sit Amet</h1>
+      </div>
+      <div className="steps">
+        {hiwsteps.map((item) => (
+          <HiwStep key={item.id} {...item} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default HowItWorksSteps;
